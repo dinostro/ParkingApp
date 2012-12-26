d81c3d668d8f033bb288921ce7a3a67c42a54bb5
Donald Inostroza <dinostroza@bonsaimt.com>
2012-12-26 18:31:20 -0300

    Se agrego mensaje toast para saludar

--- src/com/example/parkingapp/MainActivity.java
+++ src/com/example/parkingapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.parkingapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Toast.makeText(this, "Te dire donde estacionar", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
